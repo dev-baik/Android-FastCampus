2285b8f88001555f88222d0a912d542dde456ae2
wlsdn9297 <wlsdn9297@naver.com>
2023-01-05 18:11:39 +0900

    숫자세기 앱

--- chapter1/src/main/java/com/example/chapter1/MainActivity.kt
+++ chapter1/src/main/java/com/example/chapter1/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.chapter1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chapter1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var number = 0
+
+        binding.resetButton.setOnClickListener {
+            number = 0
+            binding.numberTextView.text = number.toString()
+        }
+
+        binding.plusButton.setOnClickListener {
+            number += 1
+            binding.numberTextView.text = number.toString()
+        }
     }
 }
