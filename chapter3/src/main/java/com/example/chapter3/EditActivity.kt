ff08ec0cd11e57256f663ce9e153bd6febdd3c51
wlsdn9297 <wlsdn9297@naver.com>
2023-01-06 14:53:14 +0900

    Spinner, DatePickerDialog, checkBox 기능 추가

--- chapter3/src/main/java/com/example/chapter3/EditActivity.kt
+++ chapter3/src/main/java/com/example/chapter3/EditActivity.kt
@@ -1,12 +1,44 @@
 package com.example.chapter3
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.core.view.isVisible
+import com.example.chapter3.databinding.ActivityEditBinding
 
 class EditActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.bloodTypeSpinner.adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.blood_types,
+            android.R.layout.simple_list_item_1
+        )
+
+        binding.birthDateLayer.setOnClickListener {
+            val listener = OnDateSetListener { _, year, month, dayOfMonth ->
+                binding.birthDateTextView.text = "$year-${month.inc()}-$dayOfMonth"
+            }
+            DatePickerDialog(
+                this,
+                listener,
+                2000,
+                1,
+                1
+            ).show()
+        }
+
+        binding.warningCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            binding.warningEditText.isVisible = isChecked
+        }
+
+        binding.warningEditText.isVisible = binding.warningCheckBox.isChecked
     }
 }
