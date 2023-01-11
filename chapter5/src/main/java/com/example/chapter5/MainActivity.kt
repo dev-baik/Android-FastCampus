35a2ebc72af5e2b2cb8be4ac0206625a31fa4988
wlsdn9297 <wlsdn9297@naver.com>
2023-01-11 12:43:12 +0900

    안드로이드 스레드 및 Timer 추가

--- chapter5/src/main/java/com/example/chapter5/MainActivity.kt
+++ chapter5/src/main/java/com/example/chapter5/MainActivity.kt
@@ -2,10 +2,139 @@ package com.example.chapter5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
+import com.example.chapter5.databinding.ActivityMainBinding
+import com.example.chapter5.databinding.DialogCountdownSettingBinding
+import kotlinx.coroutines.NonCancellable.start
+import java.util.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var countdownSecond = 10
+    private var currentCountdownDeciSecond = countdownSecond * 10
+    private var currentDeciSecond = 0
+    private var timer: Timer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.countDownTextView.setOnClickListener {
+            showCountDownSettingDialog()
+        }
+
+        binding.startButton.setOnClickListener {
+            start()
+            binding.startButton.isVisible = false
+            binding.stopButton.isVisible = false
+            binding.pauseButton.isVisible = true
+            binding.lapButton.isVisible = true
+        }
+
+        binding.stopButton.setOnClickListener {
+            showAlertDialog()
+        }
+
+        binding.pauseButton.setOnClickListener {
+            pause()
+            binding.startButton.isVisible = true
+            binding.stopButton.isVisible = true
+            binding.pauseButton.isVisible = false
+            binding.lapButton.isVisible = false
+        }
+
+        binding.lapButton.setOnClickListener {
+            lap()
+        }
+
+        initCountdownViews()
+    }
+
+    private fun initCountdownViews() {
+        binding.countDownTextView.text = String.format("%02d", countdownSecond)
+        binding.countDownProgressBar.progress = 100
+    }
+
+    private fun start() {
+        timer = timer(initialDelay = 0, period = 100) {
+            if (currentCountdownDeciSecond == 0) {
+                currentDeciSecond += 1
+
+                var minutes = currentDeciSecond.div(10) / 60
+                val seconds = currentDeciSecond.div(10) % 60
+                val deciSecond = currentDeciSecond % 10
+
+                runOnUiThread {
+                    binding.timeTextView.text = String.format("%02d:%02d", minutes, seconds)
+                    binding.tickTextView.text = deciSecond.toString()
+
+                    binding.countDownGroup.isVisible = false
+                }
+            } else {
+                currentCountdownDeciSecond -= 1
+                val second = currentCountdownDeciSecond / 10
+                val progress = (currentCountdownDeciSecond / (countdownSecond * 10f)) * 100
+
+                binding.root.post {
+                    binding.countDownTextView.text = String.format("%02d", second)
+                    binding.countDownProgressBar.progress = progress.toInt()
+                }
+            }
+        }
+    }
+
+    private fun stop() {
+        binding.startButton.isVisible = true
+        binding.stopButton.isVisible = true
+        binding.pauseButton.isVisible = false
+        binding.lapButton.isVisible = false
+
+        currentDeciSecond = 0
+        binding.timeTextView.text = "00:00"
+        binding.tickTextView.text = "0"
+
+        binding.countDownGroup.isVisible = true
+        initCountdownViews()
+    }
+
+    private fun pause() {
+        timer?.cancel()
+        timer = null
+    }
+
+    private fun lap() {
+
+    }
+
+    private fun showCountDownSettingDialog() {
+        AlertDialog.Builder(this).apply {
+            val dialogBinding = DialogCountdownSettingBinding.inflate(layoutInflater)
+            with(dialogBinding.countDownSecondPicker) {
+                maxValue = 20
+                minValue = 0
+                value = countdownSecond
+            }
+            setTitle("카운트다운 설정")
+            setView(dialogBinding.root)
+            setPositiveButton("확인") { _, _ ->
+                countdownSecond = dialogBinding.countDownSecondPicker.value
+                currentCountdownDeciSecond = countdownSecond * 10
+                binding.countDownTextView.text = String.format("%02d", countdownSecond)
+            }
+            setNegativeButton("취소", null)
+        }.show()
+    }
+
+    private fun showAlertDialog() {
+        AlertDialog.Builder(this).apply {
+            setMessage("종료하시겠습니까?")
+            setPositiveButton("네") { _, _ ->
+                stop()
+            }
+            setNegativeButton("아니오", null)
+        }.show()
     }
 }
