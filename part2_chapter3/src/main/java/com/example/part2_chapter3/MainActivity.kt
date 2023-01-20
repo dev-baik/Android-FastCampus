552c5b06439f797d62067b47586c91e5e6bc9787
wlsdn9297 <wlsdn9297@naver.com>
2023-01-20 12:13:16 +0900

    feat : OKHttp Client 앱 만들기

--- part2_chapter3/src/main/java/com/example/part2_chapter3/MainActivity.kt
+++ part2_chapter3/src/main/java/com/example/part2_chapter3/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.part2_chapter3
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStreamReader
 import java.io.PrintWriter
 import java.net.Socket
@@ -14,29 +16,49 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Thread {
-            try {
-                val socket = Socket("10.0.2.2", 8080)
-                val printer = PrintWriter(socket.getOutputStream())
-                val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+        val client = OkHttpClient()
 
-                printer.println("GET / HTTP/1.1")
-//              printer.println("Host: $Localhost")
-                printer.println("User-Agent: android")
-                printer.println("\r\n")
-                printer.flush()
+        val request: Request = Request.Builder()
+            .url("http://10.0.2.2:8080")
+            .build()
 
-                var input: String? = "-1"
-                while (input != null) {
-                    input = reader.readLine()
-                    Log.e("Client", input)
-                }
-                reader.close()
-                printer.close()
-                socket.close()
-            } catch (e: Exception) {
+        val callback = object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
                 Log.e("Client", e.toString())
             }
-        }.start()
+
+            override fun onResponse(call: Call, response: Response) {
+               if (response.isSuccessful) {
+                   Log.e("Client", "${response.body?.string()}")
+               }
+            }
+        }
+        client.newCall(request).enqueue(callback)
+
+
+//        Thread {
+//            try {
+//                val socket = Socket("10.0.2.2", 8080)
+//                val printer = PrintWriter(socket.getOutputStream())
+//                val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+//
+//                printer.println("GET / HTTP/1.1")
+////              printer.println("Host: $Localhost")
+//                printer.println("User-Agent: android")
+//                printer.println("\r\n")
+//                printer.flush()
+//
+//                var input: String? = "-1"
+//                while (input != null) {
+//                    input = reader.readLine()
+//                    Log.e("Client", input)
+//                }
+//                reader.close()
+//                printer.close()
+//                socket.close()
+//            } catch (e: Exception) {
+//                Log.e("Client", e.toString())
+//            }
+//        }.start()
     }
 }
