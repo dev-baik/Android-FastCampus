9afa5b5c2dec82824aae1c7319c61c69a530c9e9
wlsdn9297 <wlsdn9297@naver.com>
2023-01-20 11:19:47 +0900

    Initial commit

---
+++ part2_chapter3/src/main/java/com/example/part2_chapter3/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.part2_chapter3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.net.ServerSocket
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
