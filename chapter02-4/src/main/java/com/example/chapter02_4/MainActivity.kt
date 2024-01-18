cf0bfedcfd169314ac9411642e6dcd0e6cdf1695
dev-baik <wlsdn9297@naver.com>
2024-01-18 23:38:43 +0900

    [feat] 유저 불러오기 검색 화면 구현하기

--- chapter02-4/src/main/java/com/example/chapter02_4/MainActivity.kt
+++ chapter02-4/src/main/java/com/example/chapter02_4/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.chapter02_4
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.chapter02_4.model.Repo
+import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.chapter02_4.adapter.UserAdapter
+import com.example.chapter02_4.databinding.ActivityMainBinding
 import com.example.chapter02_4.model.UserDto
 import com.example.chapter02_4.network.GithubService
 import retrofit2.Call
@@ -13,35 +19,69 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var userAdapter: UserAdapter
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val handler = Handler(Looper.getMainLooper())
+    private var searchFor: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.github.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+//        githubService.listRepos("square").enqueue(object : Callback<List<Repo>> {
+//            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+//                Log.e("태그", response.body().toString())
+//            }
+//
+//            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+//
+//            }
+//        })
 
+        userAdapter = UserAdapter {
+            val intent = Intent(this@MainActivity, RepoActivity::class.java)
+            intent.putExtra("username", it.username)
+            startActivity(intent)
+        }
+
+        binding.userRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = userAdapter
+        }
+
+        val runnable = Runnable {
+            searchUser()
+        }
+
+        binding.searchEditText.addTextChangedListener {
+            searchFor = it.toString()
+            handler.removeCallbacks(runnable)
+            handler.postDelayed(
+                runnable,
+                300,
+            )
+        }
+    }
+
+    private fun searchUser() {
         val githubService = retrofit.create(GithubService::class.java)
-        githubService.listRepos("square").enqueue(object : Callback<List<Repo>> {
-            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
-                Log.e("태그", response.body().toString())
-            }
 
-            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
-
-            }
-        })
-
-        githubService.searchUsers("squar").enqueue(object : Callback<UserDto> {
+        githubService.searchUsers(searchFor).enqueue(object : Callback<UserDto> {
             override fun onResponse(call: Call<UserDto>, response: Response<UserDto>) {
-                Log.e("태그2", response.body().toString())
+                userAdapter.submitList(response.body()?.items)
             }
 
             override fun onFailure(call: Call<UserDto>, t: Throwable) {
-
+                Toast.makeText(this@MainActivity, "에러가 발생했습니다.", Toast.LENGTH_SHORT).show()
             }
-
         })
     }
 }
---
+++ chapter02-4/src/main/java/com/example/chapter02_4/RepoActivity.kt
@@ -0,0 +1,15 @@
+package com.example.chapter02_4
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.chapter02_4.databinding.ActivityRepoBinding
+
+class RepoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRepoBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRepoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
