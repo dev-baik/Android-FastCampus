60a99eb7c98e16b6eac8b3facfdfaedbe9afb217
dev-baik <wlsdn9297@naver.com>
2024-01-19 00:14:43 +0900

    [refactor] OkHttpClient Interceptor 추가

--- chapter02-4/src/main/java/com/example/chapter02_4/APIClient.kt
+++ chapter02-4/src/main/java/com/example/chapter02_4/APIClient.kt
@@ -1,13 +1,27 @@
 package com.example.chapter02_4
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object APIClient {
     private const val BASE_URL = "https://api.github.com/"
 
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor {
+            val request = it.request()
+                .newBuilder()
+                .addHeader("Authorization", "Bearer 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")
+                .build()
+            it.proceed(request)
+        }
+        .build()
+
     val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+
+
 }
--- chapter02-4/src/main/java/com/example/chapter02_4/network/GithubService.kt
+++ chapter02-4/src/main/java/com/example/chapter02_4/network/GithubService.kt
@@ -4,17 +4,16 @@ import com.example.chapter02_4.model.Repo
 import com.example.chapter02_4.model.UserDto
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface GithubService {
 
-    @Headers("Authorization: Bearer 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")
+//    @Headers("Authorization: Bearer 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")
     @GET("users/{username}/repos")
     fun listRepos(@Path("username") username: String, @Query("page") page: Int): Call<List<Repo>>
 
-    @Headers("Authorization: Bearer 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")
+//    @Headers("Authorization: Bearer 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")
     @GET("search/users")
     fun searchUsers(@Query("q") query: String): Call<UserDto>
 }
