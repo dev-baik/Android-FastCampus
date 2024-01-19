68570064071039145d209cbf943ecae70d770bac
dev-baik <wlsdn9297@naver.com>
2024-01-19 16:59:18 +0900

    [feat] 서버 데이터 불러오기

--- chapter02-5/src/main/java/com/example/chapter02_5/MainActivity.kt
+++ chapter02-5/src/main/java/com/example/chapter02_5/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.chapter02_5
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.tickaroo.tikxml.TikXml
+import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://news.google.com/")
+        .addConverterFactory(
+            TikXmlConverterFactory.create(
+                TikXml.Builder()
+                    .exceptionOnUnreadXml(false)
+                    .build()
+            )
+        ).build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newsService = retrofit.create(NewsService::class.java)
+        newsService.mainFeed().enqueue(object : Callback<NewsRss> {
+            override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
+                Log.e("태그", response.body().toString())
+            }
+
+            override fun onFailure(call: Call<NewsRss>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
     }
 }
---
+++ chapter02-5/src/main/java/com/example/chapter02_5/NewsService.kt
@@ -0,0 +1,10 @@
+package com.example.chapter02_5
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface NewsService {
+
+    @GET("rss?hl=ko&gl=KR&ceid=KR:ko")
+    fun mainFeed(): Call<NewsRss>
+}
