13b13857a14dd36372669b8804a58b325eefe631
dev-baik <wlsdn9297@naver.com>
2024-01-22 18:46:25 +0900

    [feat] 채팅 화면 그리기

---
+++ chapter02-6/src/main/java/com/example/chapter02_6/chatdetail/ChatActivity.kt
@@ -0,0 +1,74 @@
+package com.example.chapter02_6.chatdetail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.chapter02_6.Key.Companion.DB_CHATS
+import com.example.chapter02_6.Key.Companion.DB_USERS
+import com.example.chapter02_6.databinding.ActivityChatdetailBinding
+import com.example.chapter02_6.userlist.UserItem
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class ChatActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChatdetailBinding
+
+    private var chatRoomId: String = ""
+    private var otherUserId: String = ""
+    private var myUserId: String = ""
+
+    private val chatItemList = mutableListOf<ChatItem>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityChatdetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        chatRoomId = intent.getStringExtra("chatRoomId") ?: return
+        otherUserId = intent.getStringExtra("otherUserId") ?: return
+        myUserId = Firebase.auth.currentUser?.uid ?: ""
+
+        val chatAdapter = ChatAdapter()
+
+        Firebase.database.reference.child(DB_USERS).child(myUserId).get()
+            .addOnSuccessListener {
+                val myUserItem = it.getValue(UserItem::class.java)
+                val myUserName = myUserItem?.username
+            }
+
+        Firebase.database.reference.child(DB_USERS).child(otherUserId).get()
+            .addOnSuccessListener {
+                val otherUserItem = it.getValue(UserItem::class.java)
+                chatAdapter.otherUserItem = otherUserItem
+            }
+
+        Firebase.database.reference.child(DB_CHATS).child(chatRoomId)
+            .addChildEventListener(object : ChildEventListener {
+                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                    val chatItem = snapshot.getValue(ChatItem::class.java)
+                    chatItem ?: return
+
+                    chatItemList.add(chatItem)
+                    chatAdapter.submitList(chatItemList)
+                }
+
+                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+
+                override fun onChildRemoved(snapshot: DataSnapshot) {}
+
+                override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+
+                override fun onCancelled(error: DatabaseError) {}
+            })
+
+        binding.chatRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = chatAdapter
+        }
+    }
+}
---
+++ chapter02-6/src/main/java/com/example/chapter02_6/chatdetail/ChatItem.kt
@@ -0,0 +1,7 @@
+package com.example.chapter02_6.chatdetail
+
+data class ChatItem(
+    val chatId: String? = null,
+    val userId: String? = null,
+    val message: String? = null,
+)
