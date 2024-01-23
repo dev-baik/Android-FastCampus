23f0aa10935117f05b0e6d5843d1e062ad5e439c
dev-baik <wlsdn9297@naver.com>
2024-01-23 19:55:56 +0900

    [feat] 날씨 위젯 갱신하기

--- chapter02-7/src/main/java/com/example/chapter02_7/UpdateWeatherService.kt
+++ chapter02-7/src/main/java/com/example/chapter02_7/UpdateWeatherService.kt
@@ -1,6 +1,9 @@
 package com.example.chapter02_7
 
 import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.appwidget.AppWidgetManager
@@ -23,7 +26,8 @@ class UpdateWeatherService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
         // notification channel
-        // foregroundService
+        createChannel()
+        startForeground(1, createNotification())
 
         val appWidgetManager: AppWidgetManager = AppWidgetManager.getInstance(this)
 
@@ -32,6 +36,20 @@ class UpdateWeatherService : Service() {
                 Manifest.permission.ACCESS_BACKGROUND_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
+            val pendingIntent: PendingIntent = Intent(this, SettingActivity::class.java).let {
+                PendingIntent.getActivity(this, 2, it, PendingIntent.FLAG_IMMUTABLE)
+            }
+            RemoteViews(packageName, R.layout.widget_weather).apply {
+                setTextViewText(R.id.temperatureTextView, "권한없음")
+                setOnClickPendingIntent(R.id.temperatureTextView, pendingIntent)
+                setTextViewText(R.id.weatherTextView, "")
+            }.also { removeViews ->
+                val appWidgetName = ComponentName(this, WeatherAppWidgetProvider::class.java)
+                appWidgetManager.updateAppWidget(appWidgetName, removeViews)
+            }
+
+            stopSelf()
+
             return super.onStartCommand(intent, flags, startId)
         }
 
@@ -65,10 +83,64 @@ class UpdateWeatherService : Service() {
                     stopSelf()
                 },
                 failureCallback = {
+                    val pendingServiceIntent: PendingIntent =
+                        Intent(this, UpdateWeatherService::class.java).let { intent ->
+                            PendingIntent.getService(this, 1, intent, PendingIntent.FLAG_IMMUTABLE)
+                        }
+
+                    RemoteViews(packageName, R.layout.widget_weather).apply {
+                        setTextViewText(
+                            R.id.temperatureTextView,
+                            "에러"
+                        )
+                        setTextViewText(
+                            R.id.weatherTextView,
+                            ""
+                        )
+                        setOnClickPendingIntent(R.id.temperatureTextView, pendingServiceIntent)
+                    }.also { remoteViews ->
+                        val appWidgetName =
+                            ComponentName(this, WeatherAppWidgetProvider::class.java)
+                        appWidgetManager.updateAppWidget(appWidgetName, remoteViews)
+                    }
+
                     stopSelf()
                 }
             )
         }
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createChannel() {
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL,
+            "날씨앱",
+            NotificationManager.IMPORTANCE_LOW
+        )
+
+        channel.description = "위젯을 업데이트하는 채널"
+
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        manager.createNotificationChannel(channel)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotification(): Notification {
+        return Notification.Builder(this, NOTIFICATION_CHANNEL)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("날씨앱")
+            .setContentText("날씨 업데이트")
+            .build()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        stopForeground(STOP_FOREGROUND_REMOVE)
+    }
+
+    companion object {
+        const val NOTIFICATION_CHANNEL = "widget_refresh_channel"
     }
 }
--- chapter02-7/src/main/java/com/example/chapter02_7/WeatherAppWidgetProvider.kt
+++ chapter02-7/src/main/java/com/example/chapter02_7/WeatherAppWidgetProvider.kt
@@ -1,14 +1,19 @@
 package com.example.chapter02_7
 
+import android.app.ForegroundServiceStartNotAllowedException
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.widget.RemoteViews
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 
 class WeatherAppWidgetProvider : AppWidgetProvider() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
@@ -17,14 +22,16 @@ class WeatherAppWidgetProvider : AppWidgetProvider() {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
 
         appWidgetIds.forEach { appWidgetId ->
-            // Create an Intent to launch ExampleActivity
-            val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
+            val pendingIntent: PendingIntent = Intent(context, UpdateWeatherService::class.java)
                 .let { intent ->
-                    PendingIntent.getActivity(context, 1, intent, PendingIntent.FLAG_IMMUTABLE)
+                    PendingIntent.getForegroundService(
+                        context,
+                        1,
+                        intent,
+                        PendingIntent.FLAG_IMMUTABLE
+                    )
                 }
 
-            // Get the layout for the App Widget and attach an on-click listener
-            // to the button
             val views: RemoteViews = RemoteViews(
                 context.packageName,
                 R.layout.widget_weather
@@ -32,8 +39,19 @@ class WeatherAppWidgetProvider : AppWidgetProvider() {
                 setOnClickPendingIntent(R.id.temperatureTextView, pendingIntent)
             }
 
-            // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+
+        val serviceIntent = Intent(context, UpdateWeatherService::class.java)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            try {
+                ContextCompat.startForegroundService(context, serviceIntent)
+            } catch (e: ForegroundServiceStartNotAllowedException) {
+                e.printStackTrace()
+            }
+        } else {
+            ContextCompat.startForegroundService(context, serviceIntent)
         }
     }
 }
