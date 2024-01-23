fcde04b1fe6afe3fb3714e00f303d268d1e19c37
dev-baik <wlsdn9297@naver.com>
2024-01-23 17:14:46 +0900

    [feat] 날씨 정보 불러오기

---
+++ chapter02-7/src/main/java/com/example/chapter02_7/Forecast.kt
@@ -0,0 +1,11 @@
+package com.example.chapter02_7
+
+data class Forecast(
+    val forecastDate: String,
+    val forecastTime: String,
+
+    var temperature: Double = 0.0,
+    var sky: String = "",
+    var precipitation: Int = 0,
+    var precipitationType: String = "",
+)
--- chapter02-7/src/main/java/com/example/chapter02_7/MainActivity.kt
+++ chapter02-7/src/main/java/com/example/chapter02_7/MainActivity.kt
@@ -1,12 +1,90 @@
 package com.example.chapter02_7
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://apis.data.go.kr/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(WeatherService::class.java)
+
+        val baseDateTime = BaseDataTime.getBaseDateTime()
+        val converter = GeoPointConverter()
+        val point = converter.convert(lat = 37.5986, lon = 126.9156)
+
+        service.getVillageForecast(
+            serviceKey = "BdCsor5QNjxKxkTcr9XR+jmv3CDzcN9+vgLq2GTTkI6e9L0XVBCJPaEESxkZFTE0+v8Fzh8YBlahP82mmXjpiw==",
+            baseData = baseDateTime.baseData,
+            baseTime = baseDateTime.baseTime,
+            nx = point.nx,
+            ny = point.ny
+        ).enqueue(object : Callback<WeatherEntity> {
+            override fun onResponse(call: Call<WeatherEntity>, response: Response<WeatherEntity>) {
+                val forecastDateTimeMap = mutableMapOf<String, Forecast>()
+                val forecastList =
+                    response.body()?.response?.body?.items?.forecastEntities.orEmpty()
+
+                for (forecast in forecastList) {
+                    if (forecastDateTimeMap["${forecast.forecastDate}/${forecast.forecastTime}"] == null) {
+                        forecastDateTimeMap["${forecast.forecastDate}/${forecast.forecastTime}"] =
+                            Forecast(
+                                forecastDate = forecast.forecastDate,
+                                forecastTime = forecast.forecastTime
+                            )
+                    }
+
+                    forecastDateTimeMap["${forecast.forecastDate}/${forecast.forecastTime}"]?.apply {
+                        when (forecast.category) {
+                            Category.POP -> precipitation = forecast.forecastValue.toInt()
+                            Category.PTY -> precipitationType = transformRainType(forecast)
+                            Category.SKY -> sky = transformSky(forecast)
+                            Category.TMP -> temperature = forecast.forecastValue.toDouble()
+                            else -> {}
+                        }
+                    }
+                }
+                Log.e("태그", forecastDateTimeMap.toString())
+            }
+
+            override fun onFailure(call: Call<WeatherEntity>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
+    }
+
+    private fun transformRainType(forecast: ForecastEntity): String {
+        return when (forecast.forecastValue.toInt()) {
+            0 -> "없음"
+            1 -> "비"
+            2 -> "비/눈"
+            3 -> "눈"
+            4 -> "소나기"
+            else -> ""
+        }
+    }
+
+    private fun transformSky(forecast: ForecastEntity): String {
+        return when (forecast.forecastValue.toInt()) {
+            1 -> "맑음"
+            2 -> "구름많음"
+            3 -> "흐림"
+            else -> ""
+        }
     }
 }
