e34202e947662d734c63062a9520424d0af20e44
dev-baik <wlsdn9297@naver.com>
2024-01-28 21:57:05 +0900

    [feat] Firebase를 활용하여 이메일 로그인 구현하기

--- chapter02-10/src/main/java/com/example/chapter02_10/MainActivity.kt
+++ chapter02-10/src/main/java/com/example/chapter02_10/MainActivity.kt
@@ -1,11 +1,105 @@
 package com.example.chapter02_10
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.example.chapter02_10.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupSignUpButton()
+        setupSignInOutButton()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (Firebase.auth.currentUser == null) {
+            initViewsToSignOutState()
+        } else {
+            initViewsToSignInState()
+        }
+    }
+
+    private fun setupSignInOutButton() {
+        binding.signUpButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Snackbar.make(binding.root, "이메일 또는 패스워드를 입력해주세요.", Snackbar.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            Firebase.auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Snackbar.make(binding.root, "회원가입에 성공했습니다.", Snackbar.LENGTH_SHORT).show()
+                        initViewsToSignInState()
+                    } else {
+                        Snackbar.make(binding.root, "회원가입에 실패했습니다.", Snackbar.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
+    private fun setupSignUpButton() {
+        binding.signInOutButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (Firebase.auth.currentUser == null) {
+                // 로그인
+                if (email.isEmpty() || password.isEmpty()) {
+                    Snackbar.make(binding.root, "이메일 또는 패스워드를 입력해주세요.", Snackbar.LENGTH_SHORT)
+                        .show()
+                    return@setOnClickListener
+                }
+
+                Firebase.auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            initViewsToSignInState()
+                        } else {
+                            Snackbar.make(binding.root, "회원가입에 성공했습니다.", Snackbar.LENGTH_SHORT)
+                                .show()
+                        }
+                    }.addOnFailureListener {
+                        it.printStackTrace()
+                    }
+
+            } else {
+                // 로그아웃
+                Firebase.auth.signOut()
+                initViewsToSignOutState()
+            }
+        }
+    }
+
+    private fun initViewsToSignInState() {
+        binding.emailEditText.setText(Firebase.auth.currentUser?.email)
+        binding.emailEditText.isEnabled = false
+        binding.passwordEditText.isVisible = false
+        binding.signInOutButton.text = "로그아웃"
+        binding.signUpButton.isEnabled = false
+    }
+
+    private fun initViewsToSignOutState() {
+        binding.emailEditText.text.clear()
+        binding.emailEditText.isEnabled = true
+        binding.passwordEditText.text.clear()
+        binding.passwordEditText.isVisible = true
+        binding.signInOutButton.text = "로그인"
+        binding.signUpButton.isEnabled = true
     }
 }
