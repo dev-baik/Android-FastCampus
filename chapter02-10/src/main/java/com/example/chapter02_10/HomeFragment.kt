76e943748bf3a39c4c578ec365886ada11b99665
dev-baik <wlsdn9297@naver.com>
2024-01-29 16:30:55 +0900

    [feat] 메인 Tab UI 구성하기

---
+++ chapter02-10/src/main/java/com/example/chapter02_10/HomeFragment.kt
@@ -0,0 +1,12 @@
+package com.example.chapter02_10
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
