65efa8cce75c5c5c9587a9c86d7e62787dedbf1c
dev-baik <wlsdn9297@naver.com>
2024-01-29 16:45:00 +0900

    [feat] Firebase에서 사진 목록 가져오기

---
+++ chapter02-10/src/main/java/com/example/chapter02_10/ArticleModel.kt
@@ -0,0 +1,8 @@
+package com.example.chapter02_10
+
+data class ArticleModel(
+    val articleId: String? = null,
+    val createdAt: Long? = null,
+    val description: String? = null,
+    val imageUrl: String? = null
+)
