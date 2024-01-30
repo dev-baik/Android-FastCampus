fb0fab8843242345e17ab2bf947c2f3e217d8786
dev-baik <wlsdn9297@naver.com>
2024-01-30 18:04:15 +0900

    [feat] 홈 화면 모션 애니메이션 구현하기

--- chapter02-11/src/main/java/com/example/chapter02_11/HomeFragment.kt
+++ chapter02-11/src/main/java/com/example/chapter02_11/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.example.chapter02_11
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.chapter02_11.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
@@ -12,5 +13,19 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeBinding.bind(view)
+
+        val homeData = context?.readData() ?: return
+
+        binding.appBarTextView.text = getString(R.string.appbar_title_text, homeData.user.nickname)
+
+        binding.startCountTextView.text =
+            getString(R.string.appbar_star_title, homeData.user.starCount, homeData.user.totalCount)
+
+        binding.appBarProgressBar.progress = homeData.user.starCount
+        binding.appBarProgressBar.max = homeData.user.totalCount
+
+        Glide.with(binding.appBarImageView)
+            .load(homeData.appbarImage)
+            .into(binding.appBarImageView)
     }
 }
