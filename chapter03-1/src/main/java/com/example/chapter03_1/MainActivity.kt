8196a0fb0d0f1991e47927a5f0699a6840b72f9c
dev-baik <wlsdn9297@naver.com>
2024-02-01 22:41:56 +0900

    [feat] 얼굴 인식 모듈 연동

--- chapter03-1/src/main/java/com/example/chapter03_1/MainActivity.kt
+++ chapter03-1/src/main/java/com/example/chapter03_1/MainActivity.kt
@@ -1,22 +1,70 @@
 package com.example.chapter03_1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.PointF
+import android.graphics.RectF
+import android.os.Build
 import android.os.Bundle
+import android.util.SizeF
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.transition.TransitionManager
 import com.example.chapter03_1.databinding.ActivityMainBinding
+import com.example.face_recognition.Camera
+import com.example.face_recognition.recognition.FaceAnalyzerListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FaceAnalyzerListener {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val camera = Camera(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply {
             setContentView(root)
+            setProgressText("시작하기를 눌러주세요.")
+            camera.initCamera(binding.cameraLayout, this@MainActivity)
 
             startDetectButton.setOnClickListener {
                 it.isVisible = false
+                camera.startFaceDetect()
+                setProgressText("얼굴을 보여주세요.")
             }
         }
+    }
+
+    override fun detect() {}
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun stopDetect() {
+        camera.stopFaceDetect()
+        reset()
+    }
+
+    override fun notDetect() {}
+
+    override fun detectProgress(progress: Float, message: String) {
+        setProgressText(message)
+    }
+
+    override fun faceSize(rectF: RectF, sizeF: SizeF, pointF: PointF) {}
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        camera.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun reset() {
+        binding.startDetectButton.isVisible = true
+    }
+
+    private fun setProgressText(text: String) {
+        TransitionManager.beginDelayedTransition(binding.root)
+        binding.progressTextView.text = text
     }
 }
--- face_recognition/src/main/java/com/example/face_recognition/Camera.kt
+++ face_recognition/src/main/java/com/example/face_recognition/Camera.kt
@@ -4,15 +4,21 @@ import android.Manifest
 import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
+import androidx.core.app.ComponentActivity
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import com.example.face_recognition.recognition.FaceAnalyzer
+import com.example.face_recognition.recognition.FaceAnalyzerListener
 import com.google.common.util.concurrent.ListenableFuture
 import java.util.concurrent.Executors
 
@@ -36,8 +42,10 @@ class Camera(private val context: Context) : ActivityCompat.OnRequestPermissions
     private lateinit var previewView: PreviewView
 
     private var cameraExecutor = Executors.newSingleThreadExecutor()
+    private var listener: FaceAnalyzerListener? = null
 
-    fun initCamera(layout: ViewGroup) {
+    fun initCamera(layout: ViewGroup, listener: FaceAnalyzerListener) {
+        this.listener = listener
         previewView = PreviewView(context)
         layout.addView(previewView)
         permissionCheck(context)
@@ -57,9 +65,44 @@ class Camera(private val context: Context) : ActivityCompat.OnRequestPermissions
         cameraProviderFuture = ProcessCameraProvider.getInstance(context)
             .also { providerFuture ->
                 providerFuture.addListener({
-
+                    startPreview(context)
                 }, ContextCompat.getMainExecutor(context))
             }
+    }
+
+    fun startFaceDetect() {
+        val cameraProvider = cameraProviderFuture.get()
+        val faceAnalyzer =
+            FaceAnalyzer((context as ComponentActivity).lifecycle, previewView, listener)
+        val analysisUseCase = ImageAnalysis.Builder()
+            .build()
+            .also {
+                it.setAnalyzer(
+                    cameraExecutor,
+                    faceAnalyzer
+                )
+            }
+
+        try {
+            cameraProvider.bindToLifecycle(
+                context as LifecycleOwner,
+                cameraSelector,
+                preview,
+                analysisUseCase
+            )
+        } catch (e: Exception) {
+
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun stopFaceDetect() {
+        try {
+            cameraProviderFuture.get().unbindAll()
+            previewView.releasePointerCapture()
+        } catch (e: Exception) {
+
+        }
     }
 
     private fun startPreview(context: Context) {
