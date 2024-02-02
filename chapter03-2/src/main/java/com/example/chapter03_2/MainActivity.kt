40806f3a45485b08969ae7b643fbb25532072540
dev-baik <wlsdn9297@naver.com>
2024-02-02 17:00:28 +0900

    [feat] 보안키패드 - Shuffle 키패드 만들기

--- chapter03-2/src/main/java/com/example/chapter03_2/MainActivity.kt
+++ chapter03-2/src/main/java/com/example/chapter03_2/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.chapter03_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chapter03_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.view = this
+
+    }
+
+    fun openShuffle() {
+
+    }
+
+    fun openVerifyOtp() {
+
     }
 }
