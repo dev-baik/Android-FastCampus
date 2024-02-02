b7b1a22637aba226c6b51b4bbec9ec704678faeb
dev-baik <wlsdn9297@naver.com>
2024-02-02 23:34:30 +0900

    [feat] 휴대폰 본인인증 UI 화면 구성 (인증번호 입력)

--- chapter03-2/src/main/java/com/example/chapter03_2/IdentityInputActivity.kt
+++ chapter03-2/src/main/java/com/example/chapter03_2/IdentityInputActivity.kt
@@ -1,5 +1,6 @@
 package com.example.chapter03_2
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
@@ -116,6 +117,8 @@ class IdentityInputActivity : AppCompatActivity() {
             binding.phoneLayout.error = "전화번호 형식이 다릅니다."
             return
         }
+
+        startActivity(Intent(this, VerifyOtpActivity::class.java))
     }
 
     private fun validName() = !binding.nameEdit.text.isNullOrBlank()
