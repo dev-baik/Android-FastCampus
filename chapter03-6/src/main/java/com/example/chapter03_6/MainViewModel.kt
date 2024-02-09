4a734a67805f8b1fe235bd264bea88d60910b7e7
dev-baik <wlsdn9297@naver.com>
2024-02-09 23:55:06 +0900

    [feat] 리스트 출력하기

---
+++ chapter03-6/src/main/java/com/example/chapter03_6/MainViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.chapter03_6
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.chapter03_6.model.ListItem
+import com.example.chapter03_6.remote.repository.MainRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val mainRepository: MainRepository
+) : ViewModel() {
+
+    private val _pagingData = MutableStateFlow<PagingData<ListItem>?>(null)
+    val pagingData: StateFlow<PagingData<ListItem>?> = _pagingData
+
+    init {
+        getList()
+    }
+
+    private fun getList() {
+        viewModelScope.launch {
+            mainRepository.loadList()
+                .cachedIn(this)
+                .collectLatest { list ->
+                    _pagingData.value = list
+                }
+        }
+    }
+}
