e970f34c3d58b5edb1133b4bf00fcb85809d09ea
dev-baik <wlsdn9297@naver.com>
2024-02-10 17:26:28 +0900

    [feat] 화면 구성하기

---
+++ chapter03-7/src/main/java/com/example/chapter03_7/HiltApplication.kt
@@ -0,0 +1,8 @@
+package com.example.chapter03_7
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application() {
+}
--- chapter03-7/src/main/java/com/example/chapter03_7/MainActivity.kt
+++ chapter03-7/src/main/java/com/example/chapter03_7/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.chapter03_7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chapter03_7.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+            view = this@MainActivity
+        }
+    }
+
+    fun onClickAdd() {
+
     }
 }
